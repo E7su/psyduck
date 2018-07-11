8a69b939b602e0fd51d8ddb7b0a42215376df0bc
|K07e7su <etsu4296@gmail.com>
2018-07-11 15:36:29 +0300

    Update file.py

--- python/file.py
+++ python/file.py
@@ -1,9 +1,9 @@
-
-file = open(“testfile.txt”,”w”) 
+#!/usr/bin/python
+file = open("testfile.txt","w") 
  
-file.write(“Hello World”) 
-file.write(“This is our new text file”) 
-file.write(“and this is another line.”) 
-file.write(“Why? Because we can.”) 
+file.write("Hello World") 
+file.write("This is our new text file") 
+file.write("and this is another line.") 
+file.write("Why? Because we can.") 
  
 file.close() 
